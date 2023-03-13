ba07f9c5163a279353b62d512b08e020405f53a7
Rayane Mallek <rayane.mallek@etu.umontpellier.fr>
2023-03-13 10:16:36 +0100

    log de test

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,8 +29,7 @@ module.exports = {
                     data: { email: req.body.email, password: req.body.password }
                 };
                 await axios(options);
-                console.log('Register completed');
-                res.send('Register completed!');
+                res.json({ message: "User registered" });
             }
         } catch (error) {
             res.send('Register failed.');
